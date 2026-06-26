21f773fb2b5afe7e0ee0983f492011ac0695e0ec
Marco Paga <marco.paga@codecentric.de>
2026-06-26 14:39:08 +0200

    Markdown Preview

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -15,4 +15,13 @@ return {
       },
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npx --yes yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
