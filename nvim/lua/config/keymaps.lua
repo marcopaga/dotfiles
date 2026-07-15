b12c66006868454ae1379afd725a8f36554e7923
Marco Paga <marco.paga@codecentric.de>
2026-07-15 12:46:28 +0200

    feat: add keymap to create new file in buffer directory
    <leader>fn prompts for a filename, pre-filled with the current
    buffer's directory path.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- neue Datei im Verzeichnis des aktuellen Puffers
+vim.keymap.set("n", "<leader>fn", function()
+  local dir = vim.fn.expand("%:p:h")
+  local name = vim.fn.input("Neue Datei: ", dir .. "/")
+  if name ~= "" then
+    vim.cmd("e " .. name)
+  end
+end, { desc = "Neue Datei erstellen" })
